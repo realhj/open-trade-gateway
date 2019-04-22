8b834f7a16d05c8bd0b3381c64fe3852a3e721a6
songyachao1979 <songyachao@shinnytech.com>
2019-04-22 09:35:30 +0800

    modify ctp api for ctpse

--- src/trader_base.cpp
+++ src/trader_base.cpp
@@ -117,12 +117,14 @@ void TraderBase::OutputNotify(long notify_code, const std::string& notify_msg, c
 
 void SerializerTradeBase::DefineStruct(ReqLogin& d)
 {
-    AddItem(d.aid, "aid");
-    AddItem(d.bid, "bid");
-    AddItem(d.user_name, "user_name");
-    AddItem(d.password, "password");
-    AddItem(d.client_app_id, "client_app_id");
-    AddItem(d.client_system_info, "client_system_info");
+   	AddItem(d.aid, "aid");
+	AddItem(d.bid, "bid");
+	AddItem(d.user_name, "user_name");
+	AddItem(d.password, "password");
+	AddItem(d.client_system_info, "client_system_info");
+	AddItem(d.client_app_id, "client_app_id");	
+	AddItem(d.client_ip, "client_ip");
+	AddItem(d.client_port, "client_port");
 }
 
 void SerializerTradeBase::DefineStruct(Bank& d)
